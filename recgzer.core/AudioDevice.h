c53880ae644e9805e5314592e3f031589d75d5ad
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-07 20:34:20 +0530

    AudioDevice.h

--- recgzer.core/AudioDevice.h
+++ recgzer.core/AudioDevice.h
@@ -19,28 +19,21 @@ namespace recgzer_core {
 
 	class AudioDevice
 	{
-	private:
-		AudioDevice(IMMDevice* const);
-		AudioDevice& operator=(const AudioDevice&); // Not Implemented
-
 	public:
 		AudioDevice() {} // Empty
 		AudioDevice(const AudioDevice& other);
-
 		~AudioDevice() {} // Empty
-
-	public:
 		static AudioDevice DefaultAudioDevice();
 		static AudioDevice GetAudioDeviceById(const AudioDeviceId&);
 		static std::vector<AudioDevice> AudioDevices();
-
-	public:
 		AudioDeviceId Id() const;
 		std::wstring Name() const;
 		float PeakAudioLevel() const;
 		std::unique_ptr<WaveAudioRecorder> Recorder(AudioRecorderType) const;
 
 	private:
+	        AudioDevice(IMMDevice* const);
+		AudioDevice& operator=(const AudioDevice&); // Not Implemented
 		CComPtr<IMMDevice> audioDevice;
 		CComPtr<IAudioMeterInformation> audioMeterInformation;
 		CComPtr<IAudioSessionManager2> audioSessionManager;
